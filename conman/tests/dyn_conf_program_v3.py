4666c4ad6ae336537755d12d5980000490990d5c
Mahendra Patel <mahendra.patel@gslab.com>
2017-02-01 00:00:49 -0800

    fixing bugs in etcd v3 for environment variable

--- conman/tests/dyn_conf_program_v3.py
+++ conman/tests/dyn_conf_program_v3.py
@@ -1,21 +1,29 @@
-import etcd3,sys,os
+import etcd3,sys
+import os
 from influxdb import InfluxDBClient
 
 
-etcd = etcd3.client((os.environ.get("EW_ETCD_IP") or "localhost"), (int(os.environ.get("EW_ETCD_PORT")) or 2379))
+etcd_ip = os.environ.get("EW_ETCD_IP") or "localhost"
+etcd_port = os.environ.get("EW_ETCD_PORT") or 2379
+etcd_key_prefix = os.environ.get("EW_KEY_PREFIX_WATCH") or "except"
+etcd_log_file = os.environ.get("EW_LOG_FILE") or "watch.log"
+influx_ip = os.environ.get("EW_INFLUX_IP") or "localhost"
+influx_port = os.environ.get("EW_INFLUX_PORT") or 8086
+host_ip = os.environ.get("EW_HOST_IP") or "localhost"
 
+etcd = etcd3.client(etcd_ip, int(etcd_port))
 # watch prefix
 #watch_count = 0
 #template/testing/game
-filename = open((os.environ.get("EW_LOG_FILE") or "watch.log"), 'w+')
+#filename = open(etcd_log_file, 'a')
 prev_key = None
 prev_value = None
 neglact_duplicate = False
-events_iterator, cancel = etcd.watch_prefix((os.environ.get("EW_KEY_PREFIX_WATCH") or "except"))
+events_iterator, cancel = etcd.watch_prefix(etcd_key_prefix)
 for event in events_iterator:
     #print(type(event))
     value = None    
-    filename.write(str(event))
+    #filename.write(str(event))
     if isinstance(event,etcd3.events.PutEvent):
        #print("put event key",event.key)
        #print("put event value=",event.value)
@@ -35,8 +43,8 @@ for event in events_iterator:
        sourcePort = etcdSplitedMsg[2].split(':')[1]
        destIp = etcdSplitedMsg[3].split(':')[0]
        destPort = etcdSplitedMsg[3].split(':')[1]
-       host_ip = os.environ.get("EW_HOST_IP") or "localhost"
-       client = InfluxDBClient((os.environ.get("EW_INFLUX_IP") or "localhost"),int((os.environ.get("EW_INFLUX_PORT") or 8083)),'root','root','docker_metadata')
+       #host_ip = os.environ.get("EW_HOST_IP") or "localhost"
+       client = InfluxDBClient(influx_ip,int(influx_port),'root','root','docker_metadata')
        data = [{
                "measurement":"faultData",
                "tags":{
@@ -61,6 +69,7 @@ for event in events_iterator:
        client.write_points(data)
        prev_key = event.key
        prev_value = event.value
+       open(etcd_log_file, 'a').write(str(event))
     #watch_count += 1
     #if watch_count > 10:
     #    cancel()
